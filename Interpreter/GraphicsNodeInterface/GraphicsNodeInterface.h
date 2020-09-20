bd1cd5cb6518acab483a7ef350538b5ec90ea728
罗霄 <2362844711@qq.com>
2020-09-20 22:04:17 +0800

    fix the bug of head file mutual referencing

--- Interpreter/GraphicsNodeInterface/GraphicsNodeInterface.h
+++ Interpreter/GraphicsNodeInterface/GraphicsNodeInterface.h
@@ -2,7 +2,6 @@
 #define GRAPHICSNODEINTERFACE_H
 
 #include <QString>
-#include "Interpreter/DataModel/AbstractNode.h"
 
 using NodeName = QString;
 
@@ -10,15 +9,11 @@ class AbstractNode;
 
 class GraphicsNodeInterface{
 public:
-<<<<<<< HEAD
-	virtual NodeName getNodeName() = 0;
-	virtual void setNodeName(const NodeName&newNodeName) = 0;
-	virtual AbstractNode*  getInterpreterNode() = 0;
-=======
+
     virtual NodeName getNodeName() = 0;
     virtual void setNodeName(const NodeName&newNodeName) = 0;
     virtual class AbstractNode*getInterpreterNode() = 0;
->>>>>>> 87457d909012540b5e73e9eb33caab08ead87b99
+
 };
 
 #endif // GRAPHICSNODEINTERFACE_H
