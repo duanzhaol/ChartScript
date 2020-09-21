7a8e169ce44d12924fcf35d0046ae3f9bbf012fe
zhuqianao <3355147798@qq.com>
2020-09-21 21:05:00 +0800

    nothing

--- NodeShow/GraphicsNode/GraphicsDataNode.cpp
+++ NodeShow/GraphicsNode/GraphicsDataNode.cpp
@@ -15,25 +15,27 @@ GraphicsDataNode::GraphicsDataNode(QWidget *parent) :
   ui->setupUi(this);
   ui->comboBox->setView(new QListView());
 
+  /*设置qcombox文字居中显示，放一个lineedit进去（设置lineedit文字居中）*/
+  QLineEdit *lineEdit = new QLineEdit;
+  lineEdit->setReadOnly(true);
+  lineEdit->setAlignment(Qt::AlignCenter);
+  ui->comboBox->setLineEdit(lineEdit);
 
+  /*两个单独lineedit文字居中*/
+  ui->dataNodeName->setAlignment( Qt::AlignHCenter); //数据节点的名字居中显示
+  ui->dataNodeData->setAlignment( Qt::AlignHCenter); //数据节点的数据居中显示
+
+  /*
   QWidget::setWindowFlags(Qt::FramelessWindowHint);//设置窗口无边框
-
   //设置关于窗体为圆角：构造一个QBitmap类对象，再用QPainter类对象在bmp上绘制一个半径为20像素的圆角矩形
-
   QBitmap bmp(this->QWidget::size());
-
   bmp.fill();
-
   QPainter p(&bmp);
-
   p.setPen(Qt::NoPen);
-
   p.setBrush(Qt::blue);
-
   p.drawRoundedRect(bmp.rect(),30,30);
-
   setMask(bmp);
-
+  */
 
 }
 
---
+++ NodeShow/main.cpp
@@ -0,0 +1,37 @@
+﻿#include "GraphicsDataNode.h"
+#include "MovableInputPortProxyWidget.h"
+#include "ConnectLineItem.h"
+#include "MovableDualPortProxyWidget.h"
+
+#include <QApplication>
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include <QDebug>
+
+auto getProxy(){
+    auto node = new GraphicsDataNode;
+    auto proxy = new MovableDualPortProxyWidget;
+    proxy->setDualPortWidget(node);
+    return proxy;
+}
+
+int main(int argc,char*argv[]){
+    QApplication app(argc,argv);
+    auto scene = new QGraphicsScene;
+    auto view = new QGraphicsView;
+
+    auto p1 = getProxy(),p2 = getProxy();
+    auto line = new ConnectLineItem(p1,p2);
+
+    scene->addItem(p1);
+    scene->addItem(p2);
+    scene->addItem(line);
+
+    view->setScene(scene);
+    view->showMaximized();
+    view->setStyleSheet(R"(QGraphicsView{ background-image:url(:/img/img/beijing.jpg);})");
+    view->setViewportUpdateMode(QGraphicsView::FullViewportUpdate);
+
+
+    return app.exec();
+}
