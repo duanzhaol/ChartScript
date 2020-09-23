bb529795c352be45dd2b83045aa872f61098ff33
DuanZhaolin <762842077@qq.com>
2020-09-23 13:22:43 +0800

    啊啊啊啊啊啊啊啊啊

--- graphShow/mywindow.cpp
+++ graphShow/mywindow.cpp
@@ -3,14 +3,21 @@
 #include"widget/sceneDialogTheme.h"
 #include"widget/SceneDialogTitle.h"
 #include<QDebug>
-
+#include <QStringListModel>
 myWindow::myWindow(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::myWindow)
 {
     //ui->graphicsView->
     ui->setupUi(this);
+    QStringList theStrList; //保存初始 StringList
+    theStrList<<"北京"<<"上海"<<"天津"<<"河北"<<"山东"<<"四川"<<"重庆"<<"广东"<<"河南"; //初始化 StringList
+    auto theModel=new QStringListModel(this);
 
+            //setStringList() 函数可以初始化数据模型的字符串列表的内容
+            //也就是把数据装进这个盒子里
+    theModel->setStringList(theStrList);
+    ui->listView->setModel(theModel);
 }
 
 void myWindow::setScene(GraphicsScene *myScene)
