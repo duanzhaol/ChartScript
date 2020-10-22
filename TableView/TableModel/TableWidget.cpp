4abb4cb9b208435aa09f01f65bd454324808b486
DuanZhaolin <762842077@qq.com>
2020-10-22 09:06:21 +0800

    8

--- TableView/TableModel/TableWidget.cpp
+++ TableView/TableModel/TableWidget.cpp
@@ -102,6 +102,9 @@ void TableWidget::onCellChanged(int rows, int columns)
     list.replace(rows,newCellData);
     //arrItem->getData().replace(rows,item(rows,columns)->data(0));//将list中表格对应位置的值设为当前表格该处的值
     //showList();
+    if(rows==0){
+        arrItem
+    }
     arrItem->onDataChanged();
 }
 /**
